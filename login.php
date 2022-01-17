454f4464e1e3d5fb04f70c374c667d5d89b7a07e
“Azmi <Azmitammam@gmail.com>
2022-01-17 13:25:06 +0200

    Insert users into MySQL DB

--- login.php
+++ login.php
@@ -3,6 +3,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,20 +12,46 @@ session_start();
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <link rel="stylesheet" href="./style.css">
 </head>
-<?php 
+<?php
 
-$email = $password = "";
+$connect = mysqli_connect("localhost", "root", "", "sotre");
 
-if(isset($_POST["signupBtn"])) {
-      foreach ($_SESSION["usersData"] as $data) {
-            var_dump($data);
-            if($_POST["email"] === $_SESSION["usersData"]["email"] && $_POST["psw1"] === $_SESSION["usersData"]["password"] ) {
-                  header("Location: http://localhost/test2/dashboard.php");
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPassword = "";
+$dbName = "sotre";
+
+try {
+      $dsn = "mysql:host=" . $dbHost . ";dbname=" . $dbName;
+      $pdo = new PDO($dsn, $dbUser, $dbPassword);
+} catch (PDOException $e) {
+      echo "DB Connection Failed" . $e->getMessage();
+}
+
+$status = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $email = stripslashes($_POST['email']) ;
+      $password = stripslashes($_POST['psw1']);
+      $encryptedPassword = md5($password);
+      
+      if (empty($email) || empty($password)) {
+            $status = "All fields are required";
+      } else {
+            $email = mysqli_real_escape_string($connect, $_POST["email"]);
+            $password = mysqli_real_escape_string($connect, isset($_POST["password"]));
+            $login = "SELECT * FROM users WHERE email = '$email' AND password = '$encryptedPassword'";
+
+            $result = mysqli_query($connect, $login);
+            if (mysqli_num_rows($result) > 0) {
+
+                  header("location: http://localhost/test2/dashboard");
+            } else {
+                  echo '<script>alert("Email or Password is Incorrect")</script>';
             }
       }
 }
-
 ?>
+
 <body>
       <div class="container">
             <div class="row">
@@ -32,8 +59,8 @@ if(isset($_POST["signupBtn"])) {
                         <form action="login.php" method="POST">
                               <h2>Sign in</h2>
                               <div class="block">
-                              <input type="email" name="email" id="email" class="regInput" id="email" required placeholder="Email" onkeydown="handleEmail()">
-                              <input type="password" name="psw1" id="psw1" class="regInput" required placeholder="Password">
+                                    <input type="email" name="email" id="email" class="regInput" id="email" required placeholder="Email" onkeydown="handleEmail()">
+                                    <input type="password" name="psw1" id="psw1" class="regInput" value="" required placeholder="Password">
                               </div>
                               <button type="submit" id="signupBtn" name="signupBtn" class="signupBtn">Sign in</button>
                               <p class="haveAccountMSG">Don't have account already? <a href="./register">Sign up</a></p>
@@ -47,5 +74,5 @@ if(isset($_POST["signupBtn"])) {
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <script src="./index.js"></script>
 </body>
-</html>
 
+</html>
--- register.php
+++ register.php
@@ -8,7 +8,7 @@ session_start();
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Task2</title>
+      <title>Store</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <link rel="stylesheet" href="./style.css">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
@@ -16,65 +16,46 @@ session_start();
 
 <body>
       <?php
-      $name = $email = $psw1 = $psw2 = "";
-      $nameErrMsg = $emailErrMsg = $psw1ErrMsg = $psw2ErrMsg = $psw3ErrMsg = "";
-      $usersArray = [];
-      if (isset($_POST["signupBtn"])) {
-            $flag = false;
-            if ($flag === false) {
-                  if (empty($_POST["name"]) || strlen($_POST["name"]) < 3 || strlen($_POST["name"]) >= 16) {
-                        $nameErrMsg = "Name should be more than 4 char and less than 16 char";
-                  } else {
-                        $name = validData($_POST["name"]);
-                        $flag = true;
-                  }
-                  if (empty($_POST["email"]) || strlen($_POST["email"]) <= 9) {
-                        $emailErrMsg = "Please enter valid email address";
-                  } else {
-                        $email = validData($_POST["email"]);
-                        $flag = true;
-                  }
-                  if (empty($_POST["psw1"]) || strlen($_POST["psw1"]) <= 6) {
-                        $psw1ErrMsg = "Password should be more than 6 char";
-                  } else {
-                        $psw1 = validData($_POST["psw1"]);
-                        $flag = true;
-                  }
-                  if (empty($_POST["psw2"]) || strlen($_POST["psw2"]) <= 6) {
-                        $psw2ErrMsg = "Password should be more than 6 char";
-                  } else {
-                        $psw2 = validData($_POST["psw2"]);
-                        $flag = true;
-                  }
-                  if ($_POST["psw1"] !== $_POST["psw2"]) {
-                        $psw3ErrMsg = "Passwords doesn't match";
-                  } else {
-                        $flag = true;
-                  }
-            }
-            if ($flag === true) {
 
-                  $flag = true;
-                  $newArrayUsr = array("name" => validData($_POST["name"]), "email" => $_POST["email"], "password" => $_POST["psw1"]);
-                  array_push($usersArray, $newArrayUsr);
-                  $_SESSION["usersData"] = array(
-                        "name" => $_POST["name"],
-                        "email" => $_POST["email"],
-                        "password" => $_POST["psw1"]
-                  );
-                  $_SESSION["loginUser"] = array (
-                        "name" => $_POST["name"],
-                        "email" => $_POST["email"],
-                        "password" => $_POST["psw1"]
-                  );
-                  header("Location: http://localhost/test2/dashboard.php");
-            }
+      $dbHost = "localhost";
+      $dbUser = "root";
+      $dbPassword = "";
+      $dbName = "sotre";
+
+      try {
+            $dsn = "mysql:host=" . $dbHost . ";dbname=" . $dbName;
+            $pdo = new PDO($dsn, $dbUser, $dbPassword);
+      } catch(PDOException $e) {
+            echo "DB Connection Failed" . $e->getMessage();
       }
-      function validData($data){
-            $data = trim($data);
-            $data = stripcslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
+
+      $status = "";
+      if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $username = stripslashes($_POST['username']);
+            $email = stripslashes($_POST['email']);
+            $password = stripslashes($_POST['psw1']);
+            $passwordConfirm = stripslashes($_POST['psw2']);
+            $encryptedPassword = md5($password);
+
+            if(empty($username) || empty($email) || empty($password) || empty($passwordConfirm)) {
+                  $status = "All fields are required";
+            } else {
+                  if(strlen($username) <= 3 || strlen($username) >= 18 || !preg_match("/^[a-zA-Z'\s]+$/", $username)) {
+                        $status = "Username should be between 4 to 17 letters";
+                  } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $status = "Please enter a valid email";
+                  } else if ($password != $passwordConfirm) {
+                        $status = "Password does not match";
+                  } else if (strlen($password) <= 5 || strlen($password) >= 30 || strlen($passwordConfirm) <= 6 || strlen($passwordConfirm) >= 30) {
+                        $status = "Password should be between 6 to 30 letters";
+                  } else {
+                        $sql = "INSERT INTO users (username, password, email) VALUE (:username, :password, :email)";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt-> execute(['username' => $username, 'password' => $encryptedPassword, 'email' => $email]);
+
+                        header("Location: http://localhost/test2/login");
+                  }
+            }
       }
       ?>
 
@@ -84,20 +65,18 @@ session_start();
                         <form action="register.php" method="POST" autocomplete="off">
                               <h2>Sign up</h2>
                               <div class="block">
-                                    <input type="text" name="name" class="regInput" id="name" placeholder="Full name" onkeydown="handleInputErr()" required autocomplete="off" maxlength="23">
-                                    <input type="email" name="email" id="email" class="regInput" id="email" placeholder="Email" value="" onkeydown="handleEmail()" required autocomplete="off">
-                                    <input type="password" name="psw1" id="psw1" class="regInput" placeholder="Password" value="" onkeydown="matchingPsw(); lengthPsw();" required autocomplete="off">
-                                    <input type="password" name="psw2" id="psw2" class="regInput" placeholder="Confirm Password" value="" onkeyup="matchingPsw(); lengthPsw();" required autocomplete="off">
+                                    <input type="text" name="username" class="regInput" id="name" placeholder="Username" onsubmit="handleInputErr()" required autocomplete="off" maxlength="23">
+                                    <input type="email" name="email" id="email" class="regInput" id="email" placeholder="Email" onkeydown="handleEmail()" required autocomplete="off">
+                                    <input type="password" name="psw1" id="psw1" class="regInput" placeholder="Password"  onkeydown="matchingPsw(); lengthPsw();" required autocomplete="off">
+                                    <input type="password" name="psw2" id="psw2" class="regInput" placeholder="Confirm Password" onkeyup="matchingPsw(); lengthPsw();" required autocomplete="off">
 
                                     <span class="showpsw"><input type="checkbox" name="showPassword" id="showpsw" onclick="showPSW()"> <label for="showpsw">Show password</label></span>
                               </div>
                               <button type="submit" id="signupBtn" name="signupBtn" class="signupBtn">Sign up</button>
                               <p class="haveAccountMSG">Have account already? <a href="./login">Sign in</a></p>
-                                    <span class="errmsg" id="nameError"></span>
-                                    <span class="errmsg" id="emailError"></span>
-                                    <span class="errmsg" id="psw1Error"></span>
-                                    <span class="errmsg" id="psw2Error"></span>
-                                    <span class="errmsg" id="psw3Error"></span>
+                              <div class="form-status">
+                              <?php echo $status ?>
+                              </div>
                         </form>
                   </div>
             </div>
