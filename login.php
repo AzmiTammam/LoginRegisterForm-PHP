84e819c766fb6af713b888173562a13336d026c3
“Azmi <Azmitammam@gmail.com>
2022-01-19 10:27:45 +0200

    Add a function to check if the user is logged in

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+include('functions.php');
+if (isLoggedIn()) {
+	header('location: dashboard');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,9 +56,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 
 <body>
+      
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href=".">STORE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+      <a href="login" class="d-flex m-2">Login</a>
+      <a href="login" class="d-flex">Logout</a>
+    </div>
+  </div>
+</nav>
+
       <div class="container">
             <div class="row">
-                  <div class="col-12">
+                  <div class="col-12 loginPage">
                         <form action="login.php" method="POST">
                               <h2>Sign in</h2>
                               <div class="block">
--- register.php
+++ register.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+include('functions.php');
+if (isLoggedIn()) {
+	header('location: dashboard');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -73,9 +78,27 @@ session_start();
       }
       ?>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href=".">STORE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+      <a href="login" class="d-flex">Login</a>
+      <a href="login" class="d-flex">Logout</a>
+    </div>
+  </div>
+</nav>
+
       <div class="container">
             <div class="row">
-                  <div class="col-12">
+                  <div class="col-12 registerPage">
                         <form action="register.php" method="POST" autocomplete="off">
                               <h2>Sign up</h2>
                               <div class="block">
